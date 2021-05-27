a5a4aa694ffa00a81f229b60fb2db66a857f73ba
xiaoyou66 <xiaoyou2333@foxmail.com>
2021-05-27 16:27:15 +0800

    prefect: 完善插件功能

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ widget.addSide("更多功能","index.html",function () {
     // 不需要的自己注释掉就行了
     const functions = [
         { title: "日记",icon: "far fa-calendar-alt",link: "/diary",color: "#5FB878" },
-        { title: "赞助博主",icon: "fas fa-donate",link: "/more/donate",color: "#FFB800" },
-        { title: "友人帐",icon: "fas fa-child",link: "/more/friend",color: "#01AAED" },
+        { title: "赞助博主",icon: "fas fa-donate",link: "/more/sponsors",color: "#FFB800" },
+        { title: "友人帐",icon: "fas fa-child",link: "/more/friends",color: "#01AAED" },
         { title: "我的追番",icon: "fas fa-ghost",link: "/more/animations",color: "#f25d8e" },
     ]
     return {
